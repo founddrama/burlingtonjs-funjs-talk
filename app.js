a745f3b3077c11ac6c9e9ac11e916fa17b975557
Rob Friesel <robf@dealer.com>
2013-06-16 23:06:13 -0400

    clean-up app.js a bit...

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-/*jshint node:true*/
-var fs = require('fs');
-var gr = require('./get-shelf');
+/*jshint node:true curly:false*/
+var fs       = require('fs');
+var getShelf = require('./get-shelf').getShelf;
 
 var FILE = 'data.json';
 
-gr.getShelf('2012', function(json) {
+getShelf(process.argv[2] || '2012', function(json) {
   json = JSON.stringify(json, null, '  ');
   console.log(json);
 
   fs.writeFile(FILE, json, function(err) {
-    if (err) {
-      throw err;
-    } else {
-      console.info('Wrote to %s successfully.', FILE);
-    }
+    if (err) throw err;
+    else console.info('Wrote to %s successfully.', FILE);
   });
-
 });
