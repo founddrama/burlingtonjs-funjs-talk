586a6e5ad791ce01d2b8cc65d227f99a03dcf32f
Rob Friesel <robf@dealer.com>
2013-06-18 22:32:30 -0400

    necessary tweak to app.js that rehearsal revealed

--- app.js
+++ app.js
@@ -3,22 +3,25 @@ var fs = require('fs');
 var gr = require('./get-shelf');
 var cc = require('colorize').console;
 
-gr.getShelf(process.argv[2] || gr.DEFAULT_YEAR, function(json) {
-  json = JSON.stringify(json, null, '  ');
-  console.log(json);
+gr.getShelf(process.argv[2] || gr.DEFAULT_YEAR, function(data) {
+  var json = JSON.stringify(data, null, '  ');
 
   fs.writeFile(gr.FILE, json, function(err) {
     if (err) throw err;
-    else console.info('Wrote to %s successfully.', gr.FILE);
+    else cc.info('\nWrote #magenta[%d] reviews to #magenta[%s] successfully.', data.length, gr.FILE);
+
+    finish();
   });
 });
 
-cc.log("Got data? Great! Let's talk about functional programming in JavaScript.");
-cc.log("Run the following files as node scripts:");
-[
-  'applicative-functions.js',
-  'higher-order.js',
-  'protocols.js'
-].each(function(v) {
-  cc.log("  - #green[%s]", v);
-});
+function finish() {
+  cc.log("\nGot data? Great! Let's talk about functional programming in JavaScript.");
+  cc.log("Run the following files as node scripts:");
+  [
+    'applicative-functions.js',
+    'higher-order.js',
+    'protocols.js'
+  ].forEach(function(v) {
+    cc.log("  - #green[%s]", v);
+  });
+}
